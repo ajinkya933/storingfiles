ba6997efebb3321ebf6ecfeb56402335275dab9d
ajinkya933 <abobade@stevens.edu>
2017-11-02 11:12:42 -0500

    redirecting output of python file to sd card

--- test 2.py
+++ test 2.py
@@ -1,25 +1,8 @@
 import os
 import sys
-# the open keyword opens a file in read-only mode by default
-#f = open('/Volumes/aj/hello/hello world.txt','w')
-
-# read all the lines in the file and return them in a list
-#f.write("Foobar")
-#f.close()
-
-#with open("/Volumes/untitled/sneha/ajtext.txt", 'r') as f:
-# print (f.read())
- #f.flush()
 
 
-# The player is allowed to keep playing as long as their power is over 0.
-#x = 1
-#while True:
-#    x = x+1
-#    print(x)
-
-
-file = open("/Volumes/aj/VLP16_Points_2017-10-24-11-21-21.bag", 'w')
+'''file = open("/Volumes/aj/hello world.txt", 'w')
 x = 1
 
 while True:
@@ -27,4 +10,13 @@ while True:
     print(x)
     file.write(str(x))
     file.flush()
-   # file.close()
+   # file.close()'''
+
+
+import sys
+oldstdout = sys.stdout     # So you can restore later
+sys.stdout = open("/Volumes/aj/hello world.txt", 'w')  # Or whatever your logfile is
+for i in range(10):
+    print ("Hello", i)
+sys.stdout.close()
+sys.stdout = oldstdout
